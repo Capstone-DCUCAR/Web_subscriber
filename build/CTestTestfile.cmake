cc5d0dd4e3e7300b1ba8cd0b76fcfc662e8f1a53
SeongJu <soju0816@cu.ac.kr>
2023-04-22 19:54:04 +0900

    subscriber

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/soju/capstone_ws/src
+# Build directory: /home/soju/capstone_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("test")
