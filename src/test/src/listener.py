db70f326a7d5e9264cc9f58ea5fa3a12d30e1e9f
SeongJu <soju0816@cu.ac.kr>
2023-04-23 16:52:40 +0900

    "add pub"

--- src/test/src/listener.py
+++ src/test/src/listener.py
@@ -4,12 +4,15 @@ from std_msgs.msg import String
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    # my_topic으로 메시지를 발행
+    pub.publish(data)
 
 def listener():
+    global pub
     rospy.init_node('listener', anonymous=True)
+    pub = rospy.Publisher('point', String, queue_size=10)
     rospy.Subscriber("my_topic", String, callback)
     rospy.spin()
 
 if __name__ == '__main__':
     listener()
-
